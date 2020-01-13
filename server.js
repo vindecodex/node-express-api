bec4d5f7ecdff6976885221cf5c2785d681522ba
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-13 12:46:31 +0800

    create connection using mongoose

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const dotenv = require('dotenv').config({ path: './.env' });
+const mongoose = require('mongoose');
 const app = require('./app');
+
+// Database
+const DB = process.env.DATABASE
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => console.log('Database Connection Succeed!'));
 
 // PORT
 const port = process.env.PORT || 9000;
