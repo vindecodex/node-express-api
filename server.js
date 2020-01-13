e3961ddca20e529a32c78d578bc730ea0f0dcba1
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-13 11:15:24 +0800

    removed consolelogs

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+const dotenv = require('dotenv').config({ path: './.env' });
 const app = require('./app');
 
 // PORT
 const port = process.env.PORT || 9000;
-console.log(process.env);
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 })
