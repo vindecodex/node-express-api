762e13579ae5f2794d50f2917fdf7f85b8fcfef1
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-16 15:06:25 +0800

    fix indention

--- routes/user.js
+++ routes/user.js
@@ -5,22 +5,15 @@ const userController = require('../controllers/user');
 // Now we use Mounting Routes
 const router = express.Router();
 
-// Middleware Param
-router.param('id', userController.checkUserID);
-const chainedMiddleware = (req, res, next) => {
-  console.log('Im a chained middleware on get method in route /');
-  next();
-};
-
 router
-  .route('/')
-  .get(chainedMiddleware, userController.getUsers)
-  .post(userController.createUser);
+	.route('/')
+	.get(userController.getUsers)
+	.post(userController.createUser);
 // We Specify common routes for each methods to avoid redundant
 router
-  .route('/:id')
-  .get(userController.getUserById)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+	.route('/:id')
+	.get(userController.getUserById)
+	.patch(userController.updateUser)
+	.delete(userController.deleteUser);
 
 module.exports = router;
