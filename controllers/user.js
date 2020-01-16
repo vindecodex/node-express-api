0ada275bd0568e857edf368ea03d3a52155c2b71
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-16 15:06:10 +0800

    fix indention

--- controllers/user.js
+++ controllers/user.js
@@ -3,54 +3,54 @@ const userModel = require('./../models/user');
 
 // Handlers or Controllers
 exports.createUser = (req, res) => {
-  let genID = users.length;
-  const newUser = Object.assign({ id: genID }, req.body);
-  persons.push(newUser);
-  fs.writeFile(`${__dirname}/../data/users.json`, JSON.stringify(users), err => {
-    if (err) {
-      console.log(err);
-      return err;
-    }
-    // 201 stands for created
-    res.status(201).json({
-      status: 'success',
-      data: {
-        person: newUser
-      }
-    });
-  });
+	let genID = users.length;
+	const newUser = Object.assign({ id: genID }, req.body);
+	persons.push(newUser);
+	fs.writeFile(`${__dirname}/../data/users.json`, JSON.stringify(users), err => {
+		if (err) {
+			console.log(err);
+			return err;
+		}
+		// 201 stands for created
+		res.status(201).json({
+			status: 'success',
+			data: {
+				person: newUser
+			}
+		});
+	});
 }
 exports.getUsers = (req, res) => {
-  // sending data to browser with status code and convert data to json 
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
+	// sending data to browser with status code and convert data to json 
+	res.status(200).json({
+		status: 'success',
+		results: users.length,
+		data: {
+			users
+		}
+	});
 }
 exports.getUserById = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user
-    }
-  });
+	res.status(200).json({
+		status: 'success',
+		data: {
+			user
+		}
+	});
 }
 exports.updateUser = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      message: 'need to add logic'
-    }
-  })
+	res.status(201).json({
+		status: 'success',
+		data: {
+			message: 'need to add logic'
+		}
+	})
 }
 exports.deleteUser = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      message: 'need to add logic'
-    }
-  })
+	res.status(201).json({
+		status: 'success',
+		data: {
+			message: 'need to add logic'
+		}
+	})
 }
