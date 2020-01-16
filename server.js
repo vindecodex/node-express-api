4b2aab4886c4328e7149024c19edba99a02cdf6d
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-16 12:26:47 +0800

    separate script to models

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv').config({ path: './.env' });
 const mongoose = require('mongoose');
 const app = require('./app');
 
-// Database
+// Database Connection
 const DB = process.env.DATABASE
 mongoose.connect(DB, {
   useNewUrlParser: true,
@@ -10,51 +10,8 @@ mongoose.connect(DB, {
   useFindAndModify: false,
   useUnifiedTopology: true
 })
-  .then(() => console.log('Database Connection Succeed!'))
-  .catch(err => console.log(err));
-
-// Schema
-// Also used for validation
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'Your Name is Required']
-	},
-	age: {
-		type: Number,
-		required: [true, 'Age is Required']
-	},
-	gender: {
-		type: String,
-		required: [true, 'Gender is Required']
-	},
-	securityNumber: {
-		type: Number,
-		required: [true, 'Must have a Security Number'],
-		unique: true
-	},
-	race: {
-		type: String,
-		default: 'White'
-	}
-});
-
-// Created model
-// Model first letter must be Capital
-const User = mongoose.model('User', UserSchema);
-
-// Passing Data to DB
-const testUser = new User({
-	name: 'Vincent Villaluna',
-	age: 23,
-	gender: 'Male',
-	securityNumber: 11111
-});
-testUser.save()
-.then(doc => {
-	console.log(doc);
-})
-.catch(err =>  console.log(err));
+	.then(() => console.log('Database Connection Succeed!'))
+	.catch(err => console.log(err));
 
 // PORT
 const port = process.env.PORT || 9000;
